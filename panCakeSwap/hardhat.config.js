c64217de3ad9e46084a9d12460fda445f255911e
AshutoshMohanty13 <42117057+AshutoshMohanty13@users.noreply.github.com>
2022-02-01 15:40:10 +0530

    update hardhat.config

--- panCakeSwap/hardhat.config.js
+++ panCakeSwap/hardhat.config.js
@@ -1,8 +1,18 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
 
+const ALCHEMY_API_KEY = "A4EyEfv61x3agjnExZ990tGg0hX1tWSA";
+const ROPSTEN_PRIVATE_KEY =
+  "804163ab24d3a7fc26fee63c0b10dcfdc7bd5a9c698f0da5f6d662ab65f454be";
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.5.16",
+
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
